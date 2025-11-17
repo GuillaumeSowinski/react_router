ffeeba36018e5d6ee2ebe7ef5ad38c0a4cb224ec
Guillaume <guillaume@sowinski.fr>
2025-11-17 17:13:54 +0100

    button home page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,11 +1,21 @@
 import { useParams } from 'react-router'
+import { useNavigate } from 'react-router';
+import { Button } from 'react-bootstrap';
+
 
 
 const Category = () => {
+    const navigate = useNavigate();
     const { id } = useParams();
+
+    const handleBackToHomePage = () => {
+        navigate('/');
+    };
+
     return (
         <div>
             <h2>Vous êtes dans la catégorie : {id}</h2>
+            <Button onClick={handleBackToHomePage}>Page d’accueil</Button>
         </div>
     );
 };
