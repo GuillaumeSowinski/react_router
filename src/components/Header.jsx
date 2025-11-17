1a8cd90dd327d0c0b4e32651c362abcda767ab56
Guillaume <guillaume@sowinski.fr>
2025-11-17 13:52:52 +0100

    navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router";
+
 function Header() {
     return (
         <header>
-            <p>Header !</p>
+            <Navbar>
+                <Container as="nav">
+                    <Navbar.Toggle aria-controls="navbar" />
+                    <Navbar.Collapse id="navbar">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/">
+                                Accueil
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/categorie/vetements">
+                                Vêtements
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/categorie/chaussures">
+                                Chaussures
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
-    )
+    );
 }
 
-
-export default Header
+export default Header;
