f085f78d76153946fc1de47cf1e3635bbb987822
Guillaume <guillaume@sowinski.fr>
2025-11-17 12:11:32 +0100

    404 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Category from "./pages/Category"
+import NotFound from "./pages/NotFound"
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import { Routes, Route } from "react-router"
@@ -9,6 +10,7 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="categorie/:id" element={<Category />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   )
