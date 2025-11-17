83d3d04711de0a6abf05b3dabc0e9202a312cd82
Guillaume <guillaume@sowinski.fr>
2025-11-17 10:46:14 +0100

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import { Routes, Route } from "react-router"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+
     </Routes>
   )
 }
